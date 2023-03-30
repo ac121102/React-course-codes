9728a458d239cf905985c05236e2cc6a85969828
Arvind <ac121102@gmail.com>
2023-03-30 18:29:54 +0530

    Completed useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,11 +2,39 @@ import React, {
   useState,
   useEffect,
 } from 'react';
+
 // by default runs after every re-render
 // cleanup function
-// second parameter
+// second parameter -> Dependency Array
+// can't make useEffect an async await
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  /* runs on rendering of value */
+  useEffect(() => {
+    console.log('Effect');
+    if (value) {
+      document.title = `New Messgaes(${value})`;
+    }
+  }, [value]);
+
+  /* Only runs on re-render */
+  useEffect(() => {
+    console.log('Welcome!');
+  }, []);
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button
+        className='btn'
+        onClick={() => setValue(value + 1)}
+      >
+        Click ME!
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
