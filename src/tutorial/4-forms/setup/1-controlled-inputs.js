b2ee2844c1c9555807c2f9134e3e92875797d32a
Arvind <ac121102@gmail.com>
2023-06-02 17:55:41 +0530

    UseRef Done

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,50 +6,80 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  const [fName, setFName] = useState('');
+  const [firstName, setFirstName] = useState('');
   const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Hello');
-    console.log(e);
+    if (firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+      };
+      console.log(person);
+      setPeople((people) => {
+        return [...people, person];
+      });
+      setFirstName('');
+      setEmail('');
+    } else {
+      console.log('empty values');
+    }
   };
   return (
     <>
       <article>
         <form
           className='form'
-          // onSubmit={handleSubmit}
           onSubmit={handleSubmit}
         >
           <div className='form-control'>
             <label htmlFor='firstName'>
-              Name:
+              Name :{' '}
             </label>
             <input
               type='text'
               id='firstName'
               name='firstName'
-            ></input>
+              value={firstName}
+              onChange={(e) =>
+                setFirstName(e.target.value)
+              }
+            />
           </div>
           <div className='form-control'>
-            <label htmlFor='email'>Email:</label>
+            <label htmlFor='email'>
+              Email :{' '}
+            </label>
             <input
-              type='text'
+              type='email'
               id='email'
               name='email'
-            ></input>
+              value={email}
+              onChange={(e) =>
+                setEmail(e.target.value)
+              }
+            />
           </div>
-          <button
-            type='submit'
-            onSubmit={handleSubmit}
-          >
-            {' '}
-            Add person
+          <button type='submit'>
+            add person
           </button>
         </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person;
+          return (
+            <div
+              className='item'
+              key={id}
+            >
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </article>
-      <article></article>
     </>
   );
 };
--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -5,7 +5,40 @@ import React, { useEffect, useRef } from 'react';
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);
+  };
+
+  useEffect(() => {
+    // Doesn't trigger re-render
+    console.log(refContainer.current.value);
+    refContainer.current.focus();
+  });
+
+  return (
+    <>
+      <form
+        className='form'
+        onSubmit={handleSubmit}
+      >
+        <input
+          type='text'
+          ref={refContainer}
+        />
+        <button
+          type='submit'
+          className='btn'
+        >
+          Submit
+        </button>
+      </form>
+      <div ref={divContainer}>Hello World</div>
+    </>
+  );
 };
 
 export default UseRefBasics;
